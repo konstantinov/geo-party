bd808b4f3b8fe658669a3945ac4546d1de987a51
Dmitry Konstantinov <konstantinov.dmitry@gmail.com>
2023-05-26 02:12:37 +0300

    Add google OAuth backend implementation

--- src/src/lib/db.js
+++ src/src/lib/db.js
@@ -2,14 +2,22 @@ import mongoose from 'mongoose';
 
 const { Schema, model } = mongoose;
 
-export const db = await mongoose.connect('mongodb+srv://mongo/geo');
+export const db = await mongoose.connect('mongodb://127.0.0.1:27020/geo');
 
 const userSchema = new Schema({
 	name: String,
-	email: String,
-	googleId: String,
-	createdAt: Date,
+	email: { type: String, unique: true },
+	googleId: { type: String, unique: true },
+	avatar: String,
+	createdAt: { type: Date, default: () => new Date() },
 	updatedAt: Date
 });
 
-export const User = model('userrs', userSchema);
+const sessionSchema = new Schema({
+	userId: { type: mongoose.Types.ObjectId, index: true },
+	expiresAt: Date,
+	createdAt: { type: Date, default: () => new Date() }
+});
+
+export const User = model('users', userSchema);
+export const Session = model('sessions', sessionSchema);
--- src/src/routes/profile/login/+server.js
+++ src/src/routes/profile/login/+server.js
@@ -1,14 +1,31 @@
 import axios from 'axios';
 
+import { PUBLIC_SESSION_TTL } from '$env/static/public';
+import { User, Session } from '$lib/db';
+
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ cookies, request }) {
 	const { accessToken } = await request.json();
-	console.log(accessToken);
 	const { data } = await axios.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json', {
 		headers: { Authorization: `Bearer ${accessToken}` }
 	});
-	console.log(data);
-	cookies.set('sid', '123', {
+	const existsingsUser = await User.findOne({ googleId: data.id }).exec();
+
+	const { _id: userId } =
+		existsingsUser ||
+		(await User.create({
+			name: data.name,
+			email: data.email,
+			googleId: data.id,
+			avatar: data.picture
+		}));
+
+	const { _id: sessionId } = await Session.create({
+		userId,
+		expiresAt: Date.now() + parseInt(PUBLIC_SESSION_TTL) * 1000
+	});
+
+	cookies.set('sid', sessionId.toString(), {
 		path: '/',
 		httpOnly: true,
 		expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
