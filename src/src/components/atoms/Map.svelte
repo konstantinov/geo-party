27b653876f554f1c6eb6be03207b29a4c351101c
Dmitry Konstantinov <konstantinov.dmitry@gmail.com>
2023-06-02 00:24:50 +0300

    Map component refactoring

--- src/src/components/atoms/Map.svelte
+++ src/src/components/atoms/Map.svelte
@@ -1,24 +1,40 @@
 <script>
+	import { PUBLIC_YANDEX_API_KEY } from '$env/static/public';
+	import MapContent from '~/atoms/MapContent.svelte';
 	import { onMount } from 'svelte';
 
-	export let center;
 	export let containerClass;
+	let script;
+	let mapsInitialized = false;
+	const scriptNeeded = typeof ymaps === 'undefined';
 
-	let map, container;
-
-	$: {
-		if (map && center) {
-			map.setLocation({ center: [center.lat, center.lon], duration: 200 });
-		}
-	}
+	const initMaps = () => {
+		ymaps.ready(() => {
+			mapsInitialized = true;
+		});
+	};
 
 	onMount(async () => {
-		map = new ymaps.Map(container, {
-			center: center || [37.64, 55.76],
-			zoom: 7,
-			controls: ['geolocationControl', 'typeSelector']
-		});
+		if (scriptNeeded) {
+			await new Promise((resolve) => {
+				script.addEventListener('load', () => {
+					initMaps();
+				});
+			});
+		} else {
+			initMaps();
+		}
 	});
 </script>
 
-<div bind:this={container} class={containerClass} />
+<svelte:head>
+	{#if scriptNeeded}
+		<script
+			bind:this={script}
+			src="https://api-maps.yandex.ru/2.1/?apikey={PUBLIC_YANDEX_API_KEY}&lang=en_GB"
+		></script>
+	{/if}
+</svelte:head>
+{#if mapsInitialized}
+	<MapContent {containerClass} />
+{/if}
--- src/src/routes/map/+page.svelte
+++ src/src/routes/map/+page.svelte
@@ -1,40 +1,5 @@
 <script>
-	import { PUBLIC_YANDEX_API_KEY } from '$env/static/public';
 	import Map from '~/atoms/Map.svelte';
-	import { onMount } from 'svelte';
-
-	let script;
-	let mapsInitialized = false;
-	let mapCenter;
-	const scriptNeeded = typeof ymaps === 'undefined';
-
-	const initMaps = () => {
-		ymaps.ready(() => {
-			mapsInitialized = true;
-		});
-	};
-
-	onMount(async () => {
-		if (scriptNeeded) {
-			await new Promise((resolve) => {
-				script.addEventListener('load', () => {
-					initMaps();
-				});
-			});
-		} else {
-			initMaps();
-		}
-	});
 </script>
 
-<svelte:head>
-	{#if scriptNeeded}
-		<script
-			bind:this={script}
-			src="https://api-maps.yandex.ru/2.1/?apikey={PUBLIC_YANDEX_API_KEY}&lang=en_GB"
-		></script>
-	{/if}
-</svelte:head>
-{#if mapsInitialized}
-	<Map containerClass="std-f" />
-{/if}
+<Map containerClass="std-f" />
